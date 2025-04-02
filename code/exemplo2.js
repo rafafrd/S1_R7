96b54cb9266b7c8af2935c8dcbcdaed3ecdb4a79
rafafrd <bolekao03@gmail.com>
2025-04-02 09:48:39 -0300

    add features

--- code/exemplo2.js
+++ code/exemplo2.js
@@ -1,4 +1,16 @@
 //Verifica que tipo de triangulo o usuario inseriu
-let lado1 = parseFloat(prompt("Digite o primeiro lado: "))
-let lado2 = parseFloat(prompt("Digite o segundo lado: "))
-let lado3 = parseFloat(prompt("Digite o terceiro lado: "))
+let lado1 = parseFloat(prompt("Digite o valor do primeiro lado: "));
+let lado2 = parseFloat(prompt("Digite o valor do segundo lado: "));
+let lado3 = parseFloat(prompt("Digite o valor do terceiro lado: "));
+if (isNaN(lado1 && lado2 && lado3)) {
+  //Verifica se o user atribuiu números
+  alert("Você precisa digitar um número, tente novamente! 🔁");
+} else {
+  if (lado1 == lado2 && lado2 == lado3) {
+    alert("Triângulo equilatero");
+  } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3){
+    alert("Triângulo isósceles")
+  } else {
+    alert("Triângulo Escaleno")
+  }
+}
