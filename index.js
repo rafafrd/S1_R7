82bbf98ccedba1ac69696c49bfcc4d575fe91eff
rafafrd <bolekao03@gmail.com>
2025-04-02 08:48:14 -0300

    add features

---
+++ index.js
@@ -0,0 +1,17 @@
+let n1 = parseFloat(prompt("Digite o primeiro número: "));
+let n2 = parseFloat(prompt("Digite o segundo numero"));
+if (isNaN(n1)) {
+  alert("Você precisa digitar um número, tente novamente! 🔁");
+}
+if (!isNaN(n1)) {
+  if (isNaN(n2)) {
+    alert("Você precisa digitar um número, tente novamente! 🔁");
+  }
+  if (!isNaN(n2)) {
+    if (n1 == n2) {
+      alert("Os números são iguais!");
+    }else{
+      alert("Os números não são iguais!")
+    }
+  }
+}
